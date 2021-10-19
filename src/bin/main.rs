76c177febbf47c10efc65d617ed976a26fbd6bb2
danielcarney96 <dancarney@live.co.uk>
2021-10-19 23:26:15 +0100

    Gracefully shutdown.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,15 +7,17 @@ use http_server::ThreadPool;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    match ThreadPool::new(4) {
+    match ThreadPool::new(2) {
         Ok(pool) => {
-            for stream in listener.incoming() {
+            for stream in listener.incoming().take(2) {
                 let stream = stream.unwrap();
         
                 pool.execute(|| {
                     handle_connection(stream);
                 });
             }
+
+            println!("Shutting down.");
         },
         Err(_err) => println!("Pool failed to create")
     }
